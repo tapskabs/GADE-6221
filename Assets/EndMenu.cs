69ddbde058e73ca869f8634f51efaf0de240d508
tapskabs <141030379+tapskabs@users.noreply.github.com>
2024-03-31 18:03:14 +0200

    Start and End Screen

---
+++ Assets/EndMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EndMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
