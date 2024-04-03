d01dcca48d174ea8df3916f1fa75eb10f1ac1732
tapskabs <141030379+tapskabs@users.noreply.github.com>
2024-04-03 19:42:47 +0200

    FINAL PART 1
    FINAL PART 1

--- Assets/EndMenu.cs
+++ Assets/EndMenu.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 public class EndMenu : MonoBehaviour
 {
+   
+    
     public void QuitGame()
     {
         Application.Quit();
--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -1,43 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 public class PlayerScore : MonoBehaviour
 {
 
-    private int score = 0;
+    public int score = 0;
+    public static PlayerScore instan;
     public TextMeshProUGUI scoreText;
+    //public TextMeshProUGUI EndScoreText;
+    public TextMeshProUGUI deathText;
+    public int value;
+    public static PlayerScore instance;
+
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
             // Increase the score by 1
-            score++;
-
-            scoreText.text = ("Score: " + score).ToString();
+            Score();
             
            // Debug.Log("Score: " + score);
 
             // Destroy the coin
             Destroy(other.gameObject);
+            
+            //Debug.Log("EndScore: " + endscore);
+            //endScoreText.text = ("Score: " + score).ToString();
         }
-
 
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy Head"))
         {
-            score++;
-            score++;
-            scoreText.text = ("Score: " + score).ToString();
+            Score();
             // Update the UI or display the score in some way
             Debug.Log("Score: " + score);
-
+            // endScoreText.text = ("Score: " + endScore).ToString();
             
-            
+            //Debug.Log("End Score: " + endscore);
+            //endScoreText.text = ("Score: " + endscore).ToString();
         }
     }
+   public void Score()
+    {
+        score += value;
+        score++;
+        scoreText.text = ("Score: " + score).ToString();
+
+        
+
+    }
+    public void UpdateDeathScore()
+    {
+        deathText.text = "Score: " + score;
+        PlayerPrefs.SetInt("ScoreSave", score);
+    }
+     void start()
+    {
+        value = PlayerPrefs.GetInt("ScoreSave", score);
+        deathText.text = "Score: " + value;
+    }
+
 }
